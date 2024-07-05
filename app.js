6bd25a7ba6fa750c5f9139f7dc25b3e8ef0d2d04
ZyBryx <zybryxm@gmail.com>
2024-07-05 23:53:11 +0800

    Google OAuth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
 const cors = require("cors");
+const passport = require("./passportGoogleOAuth");
 
 const DB_URL = process.env.DB_URI;
 const PORT = 3000;
@@ -19,6 +20,7 @@ const notFound = require("./middleware/not-found");
 const { authMiddleware } = require("./middleware/authMiddleware");
 
 app.use(express.json());
+app.use(passport.initialize());
 app.use(
   cors({
     origin: true,
@@ -28,6 +30,30 @@ app.use(
 
 app.get("/test", (req, res) => {
   res.json("Test");
+});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/data",
+    failureRedirect: "/login",
+  })
+);
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+app.get("/data", isLoggedIn, (req, res) => {
+  const userEmail = req.user.emails[0].value;
+
+  res.send("Data sent successfully!");
 });
 
 app.use("/api/v1/auth", authRoute);
